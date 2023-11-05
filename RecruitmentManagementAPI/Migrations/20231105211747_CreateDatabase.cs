8bdd63a3e9e618b7f870c41c426301f4e24bbd01
jeronimoGD <jeronimo.garciadelgado@gmail.com>
2023-11-05 22:50:18 +0100

    [Ref #2] Add Data base migrations to create the initial version of the database.

---
+++ RecruitmentManagementAPI/Migrations/20231105211747_CreateDatabase.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecruitmentManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recruiters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recruiters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecruiterId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Candidates_Recruiters_RecruiterId",
+                        column: x => x.RecruiterId,
+                        principalTable: "Recruiters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CsvDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RecruiterId = table.Column<int>(type: "int", nullable: true),
+                    CandidateId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CsvDocuments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CsvDocuments_Candidates_CandidateId",
+                        column: x => x.CandidateId,
+                        principalTable: "Candidates",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CsvDocuments_Recruiters_RecruiterId",
+                        column: x => x.RecruiterId,
+                        principalTable: "Recruiters",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Recruiters",
+                columns: new[] { "Id", "CreationTime", "Email", "Name", "Password", "Rol", "UpdateTime" },
+                values: new object[] { 1, new DateTime(2023, 11, 5, 21, 17, 46, 608, DateTimeKind.Utc).AddTicks(8603), "defaultRecruiter@gmail.com", "DefaultRecruiter", "password", "recruiter", new DateTime(2023, 11, 5, 21, 17, 46, 608, DateTimeKind.Utc).AddTicks(8609) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_RecruiterId",
+                table: "Candidates",
+                column: "RecruiterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CsvDocuments_CandidateId",
+                table: "CsvDocuments",
+                column: "CandidateId",
+                unique: true,
+                filter: "[CandidateId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CsvDocuments_RecruiterId",
+                table: "CsvDocuments",
+                column: "RecruiterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CsvDocuments");
+
+            migrationBuilder.DropTable(
+                name: "Candidates");
+
+            migrationBuilder.DropTable(
+                name: "Recruiters");
+        }
+    }
+}
