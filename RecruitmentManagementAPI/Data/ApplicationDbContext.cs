c8c027cbc503ea4c9ee8a117cd5face188234f6e
jeronimoGD <jeronimo.garciadelgado@gmail.com>
2023-11-05 22:34:26 +0100

    [Ref #2] Create Data Base context con las diferenes entidades de Recruiters, candidates and CSVDocuments. Se crea un usuario Recruiter por defecto.

---
+++ RecruitmentManagementAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecruitmentManagementAPI.Models.Entities;
+
+namespace RecruitmentManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Recruiter> Recruiters { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<CSVDocument> CsvDocuments{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recruiter>().HasData(
+                new Recruiter()
+                {
+                    Id = 1,
+                    Name = "DefaultRecruiter",
+                    Email = "defaultRecruiter@gmail.com",
+                    Password = "password",
+                    Rol = "recruiter",
+                    CreationTime = DateTime.UtcNow,
+                    UpdateTime = DateTime.UtcNow }
+                );
+        }
+    }
+}
